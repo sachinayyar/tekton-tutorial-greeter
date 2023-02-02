edb0a38e86eec484ac74aeb5391fd422196179b7
Sachin Ayyar <91127350+sachinayyar@users.noreply.github.com>
2023-02-02 15:35:30 +0530

    Update GreetingResource.java

--- quarkus/src/main/java/com/redhat/developers/GreetingResource.java
+++ quarkus/src/main/java/com/redhat/developers/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String hello() {
-    return "Meeow!! from Tekton 😺🚀";
+    return "Meeow!! from Tekton cat 😺🚀";
   }
 }
 
