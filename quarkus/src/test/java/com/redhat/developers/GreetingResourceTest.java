ef2d68e28dd6f963ff2678c1dc1672ca7c1a7370
Sachin Ayyar <91127350+sachinayyar@users.noreply.github.com>
2023-02-02 12:12:45 +0530

    Update GreetingResourceTest.java

--- quarkus/src/test/java/com/redhat/developers/GreetingResourceTest.java
+++ quarkus/src/test/java/com/redhat/developers/GreetingResourceTest.java
@@ -11,7 +11,7 @@ public class GreetingResourceTest {
 
   @Test
   public void testHelloEndpoint() {
-    given().when().get("/").then().statusCode(200).body(is("Meeeeeeeoooowwwww!! from Tekton 😺🚀"));
+    given().when().get("/").then().statusCode(200).body(is("Meeow!! from Tekton 😺🚀"));
   }
 
 }
